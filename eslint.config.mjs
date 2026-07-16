f5e8e87df973327675e79d94a05ffa1ee803a9c7
Sanju Dhritlahre <romeyoboy765@gmail.com>
2026-07-17 01:21:10 +0530

    Integrating ESLint into Workflow

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+
+export default defineConfig({
+  files: ["**/*.{js,ts}"],
+  extends: [js.configs.recommended, tseslint.configs.recommendedTypeChecked],
+  languageOptions: {
+    parser: tseslint.parser,
+    parserOptions: {
+      projectService: true,
+    },
+  },
+  ignores: ["**/node_modules/**", "dist/**", "eslint.config.mjs"],
+  rules: {
+    "no-console": "error",
+  }
+});
