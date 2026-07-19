21dd9937437992307632a4c1a99b6a64e67c85b5
Sanju Dhritlahre <romeyoboy765@gmail.com>
2026-07-20 01:05:13 +0530

    Setting Up a Logger

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import app from "./app.ts";
 import { Config } from "./config/index.ts";
+import logger from "./config/logger.ts";
 
 const startServer = () => {
   const PORT = Config.PORT;
   try {
     app.listen(PORT, () => {
-      console.log(`Server running at port: http://localhost:${PORT}`);
+      logger.info(`Server running at port: http://localhost:${PORT}`, {
+        port: PORT,
+      });
     });
   } catch (err) {
     console.error(err);
