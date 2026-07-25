8c4df8f610fd537664efb7c3dfb36aa870a13cd7
Sanju Dhritlahre <romeyoboy765@gmail.com>
2026-07-26 02:20:11 +0530

    Write First Test - User Registration

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   testEnvironment: "node",
   roots: ["<rootDir>"],
   testMatch: ["**/?(*.)+(spec|test).[tj]s?(x)"],
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   transform: {
     "^.+\\.tsx?$": [
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express, { NextFunction, Request, Response } from "express";
 import type { HttpError } from "http-errors";
 import logger from "./config/logger";
+import authRouter from "./routes/auth.route";
 
 const app = express();
 
 app.get("/", (_req, res) => {
   res.send("Welcome to Rasoify Auth Service!");
 });
+
+app.use("/auth", authRouter);
 
 // Global Error Handler
 app.use((err: HttpError, _req: Request, res: Response, next: NextFunction) => {
